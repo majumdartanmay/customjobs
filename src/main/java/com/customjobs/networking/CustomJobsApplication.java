1922dc244356c302c005a28a04ecd4e8f49ccff2
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2019-12-07 20:34:11 +0530

    initial commit

---
+++ src/main/java/com/customjobs/networking/CustomJobsApplication.java
@@ -0,0 +1,13 @@
+package com.customjobs.networking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customjobs/networking/CustomJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customjobs.networking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
